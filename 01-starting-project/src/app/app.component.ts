5677fe43b3de0763d6d9ea7f3df4d18628c8db15
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-24 10:02:32 +0300

    required and optional inputs

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +12,5 @@ import { UserComponent } from './user/user.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // ...
+  users = DUMMY_USERS;
 }
