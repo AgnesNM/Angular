976336d64089ebf140a088cae8e240bf415ddba3
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-25 20:27:43 +0300

    Migrating all components to use modules

--- 02-starting-project/src/app/app.module.ts
+++ 02-starting-project/src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser'
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import { TasksComponent } from './tasks/tasks.component';
 import { UserComponent } from './user/user.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-    imports:[BrowserModule, HeaderComponent, TasksComponent, UserComponent]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+    CardComponent,
+    UserComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, FormsModule],
 })
-export class AppModule {};
+export class AppModule {}
