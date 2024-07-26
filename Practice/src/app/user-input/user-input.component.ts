b805673b88ee1d9fd1aca46533709ca2e0ba3912
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-26 21:40:18 +0300

    two-way binding

--- Practice/src/app/user-input/user-input.component.ts
+++ Practice/src/app/user-input/user-input.component.ts
@@ -9,7 +9,16 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  enteredInitialInvestment: string = '0';
+  enteredAnnualInvestment: string = '0';
+  enteredExpectedReturn : string = '5';
+  enteredDuration: string = '10'
+  
   onSubmit(){
     console.log("SUBMITTED");
+    console.log(this.enteredInitialInvestment);
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredExpectedReturn);
+    console.log(this.enteredDuration);
   }
 }
