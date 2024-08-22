b1e857576baab02f2c749b27266faa5aa5674d0c
AgnesNM <agnesndutamwaniki@gmail.com>
2024-08-22 16:39:56 +0300

    making user inputs more dynamic

--- form_inputs.js
+++ form_inputs.js
@@ -1,15 +1,35 @@
 import angular from 'angular';
 
+// angular.module('plunker', [])
+// .controller('StartUpController', function($scope) {
+//   $scope.funding = {
+//     startingEstimate: 0,
+//     needed: 0
+//   };
+
+//   $scope.computeNeeded = function() {
+//     $scope.funding.needed = $scope.funding.startingEstimate * 10;
+//   };
+
+//   $scope.$watch('funding.startingEstimate', computeNeeded);
+// });
+
 angular.module('plunker', [])
 .controller('StartUpController', function($scope) {
-  $scope.funding = {
-    startingEstimate: 0,
-    needed: 0
+  $scope.computeNeeded = function (){
+    $scope.needed = $scope.startingEstimate * 10;
   };
 
-  $scope.computeNeeded = function() {
-    $scope.funding.needed = $scope.funding.startingEstimate * 10;
+  $scope.requestFunding = function (){
+    if ($scope.startingEstimate > 100){
+      window.alert($scope.needed);
+    }
+    else {
+      window.alert("Please get more customers first.");
+    }
   };
 
-  $scope.$watch('funding.startingEstimate', computeNeeded);
+  $scope.reset = function (){
+    $scope.startingEstimate = 0;
+  };
 });
